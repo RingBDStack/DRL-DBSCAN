f3954e4c15fdfb20b7731cb108470e03e72cc17b
FeiLongb <74604608+FeiLongb@users.noreply.github.com>
2022-08-23 11:18:16 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -20,6 +20,7 @@ def load_data_shape(data, train_size):
     :return: sample serial numbers for rewards, out-of-order data features and labels
     """
 
+    # extract features and labels of the dataset
     extract_data = []
     with open(data, 'r') as f:
         for line in f:
@@ -31,9 +32,11 @@ def load_data_shape(data, train_size):
 
     # shuffle the samples
     extract_data_idx = random.sample(extract_data, len(extract_data))
+
     # feature normalization
     extract_data_features = np.array(MinMaxScaler().fit_transform([i[0:2] for i in extract_data_idx]))
     extract_data_labels = np.array([i[2] for i in extract_data_idx])
+
     # sample index for reward
     extract_data_train_masks = random.sample(range(len(extract_data_idx)),
                                              int(len(extract_data_idx) * train_size))
@@ -51,6 +54,7 @@ def load_data_stream(data, train_size, block_num, block_size):
     :return: sample serial numbers for rewards, out-of-order data features and labels
     """
 
+    # extract features and labels of the dataset
     extract_datas = []
     with open(data, 'r') as f:
         i = 0
@@ -61,17 +65,18 @@ def load_data_stream(data, train_size, block_num, block_size):
             extract_datas.append(data)
             i += 1
             if i == block_num * block_size: break
-
     extract_datas = [extract_datas[block_size*i:block_size*(i+1)] for i in range(block_num)]
 
     extract_data_train_maskss, extract_data_featuress, extract_data_labelss = [], [], []
     for extract_data in extract_datas:
         # shuffle the samples
         extract_data_idx = random.sample(extract_data, len(extract_data))
+
         # feature normalization
         extract_data_features = np.array(
             MinMaxScaler().fit_transform([i[0:len(extract_data[0]) - 1] for i in extract_data_idx]))
         extract_data_labels = np.array([i[-1] for i in extract_data_idx])
+
         # sample index for reward
         extract_data_train_masks = random.sample(range(len(extract_data_idx)),
                                                  int(len(extract_data_idx) * train_size))
@@ -90,7 +95,7 @@ def generate_parameter_space(features, num_layer, eps_size, min_size, data_path)
     :param eps_size: size of the eps parameter space in each recursive layer
     :param min_size: size of the min parameter space in each recursive layer
     :param data_path: path of dataset, used to determine the maximum search range of MinPts
-    :return: size per layer, step size per layer, starting point of the first layer,
+    :return: parameter space size per layer, step size per layer, starting point of the first layer,
     limit bound for the two parameter spaces
     """
 
@@ -99,16 +104,21 @@ def generate_parameter_space(features, num_layer, eps_size, min_size, data_path)
         mm = 4
     elif "Stream" in data_path:
         mm = 400
-    # feature dimension
+
+    # feature dimension and dataset size
     dim = len(features[0])
     num = len(features)
+
     # parameter space size
     p_size = [eps_size, min_size]
+
     # parameter search step
     p_step = [[(int(math.sqrt(dim)) / eps_size ** i), math.ceil(int(num/mm) / min_size ** i)]
               for i in range(1, num_layer + 1)]
+
     # parameter starting point
     p_center = [int(math.sqrt(dim)) / 2, math.ceil(int(num/mm) / 2)]
+
     # parameter space boundary
     p_bound = [[0.0000001, int(math.sqrt(dim))], [1, int(num/mm)]]
 
@@ -116,10 +126,22 @@ def generate_parameter_space(features, num_layer, eps_size, min_size, data_path)
 
 
 def kmeans_metrics(features, labels):
+    """
+    the evaluate function of clustering algorithm
+    :param features: data features
+    :param labels: accurate labels
+    :return: normalized mutual info
+    """
 
+    # n_clusters
     extract_cluster_num = len(set(labels.tolist()))
+
+    # Dataset size
     extract_data_num = features.shape[0]
+
+    # Clustering results of Kmeans
     k_labels = KMeans(n_clusters=extract_cluster_num).fit(features).labels_
+
     k_nmi = round(metrics.normalized_mutual_info_score(labels, k_labels), 4)
     k_ami = round(metrics.adjusted_mutual_info_score(labels, k_labels), 4)
     k_ari = round(metrics.adjusted_rand_score(labels, k_labels), 4)
@@ -137,6 +159,12 @@ def kmeans_metrics(features, labels):
 
 
 def dbscan_metrics(true_labels, cur_labels):
+    """
+    the evaluate function of clustering algorithm
+    :param true_labels: accurate labels
+    :param cur_labels: clustering results
+    :return: normalized mutual info, adjusted mutual info, adjusted rand info
+    """
 
     d_nmi = round(metrics.normalized_mutual_info_score(true_labels, cur_labels), 4)
     d_ami = round(metrics.adjusted_mutual_info_score(true_labels, cur_labels), 4)
